fb22aad24778508bf6247eaa82de27a1204997ec
Kieran7741 <kieran7741@gmail.com>
2019-06-08 16:41:41 +0100

    added reducer

--- src/componets/Contact.js
+++ src/componets/Contact.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { Consumer } from '../context';
+
 class Contact extends Component {
     // Note you do not need to bind 'this' when using an arrow function
     state = {
@@ -10,46 +12,60 @@ class Contact extends Component {
     onShowClick = event => {
         this.setState({ showContactInfo: !this.state.showContactInfo });
     };
-    onDeleteClick = () => {
-        this.props.deleteClickHandler();
-        console.log('Delete pressed');
+    onDeleteClick = (id, dispatch) => {
+        dispatch({ type: 'DELETE_CONTACT', payload: id });
     };
     render() {
-        const { name, email, phone } = this.props.contact;
+        const { name, email, phone, id } = this.props.contact;
         const { showContactInfo } = this.state;
+
         return (
-            <div className="card card-body mb-3">
-                <h4>
-                    Contact: {name}{' '}
-                    <i
-                        onClick={this.onShowClick}
-                        className="fas fa-caret-down"
-                        style={{ cursor: 'pointer' }}
-                    />
-                    <i
-                        onClick={this.onDeleteClick}
-                        className="fas fa-trash-alt"
-                        style={{
-                            cursor: 'pointer',
-                            float: 'right',
-                            color: 'red'
-                        }}
-                    />
-                </h4>
-                {showContactInfo ? (
-                    <ul className="list-group">
-                        <li className="list-group-item">Eamil: {email}</li>
-                        <li className="list-group-item">Phone: {phone}</li>
-                    </ul>
-                ) : null}
-            </div>
+            <Consumer>
+                {value => {
+                    const { dispatch } = value;
+                    return (
+                        <div className="card card-body mb-3">
+                            <h4>
+                                Contact: {name}{' '}
+                                <i
+                                    onClick={this.onShowClick}
+                                    className="fas fa-caret-down"
+                                    style={{ cursor: 'pointer' }}
+                                />
+                                <i
+                                    onClick={this.onDeleteClick.bind(
+                                        this,
+                                        id,
+                                        dispatch
+                                    )}
+                                    className="fas fa-trash-alt"
+                                    style={{
+                                        cursor: 'pointer',
+                                        float: 'right',
+                                        color: 'red'
+                                    }}
+                                />
+                            </h4>
+                            {showContactInfo ? (
+                                <ul className="list-group">
+                                    <li className="list-group-item">
+                                        Eamil: {email}
+                                    </li>
+                                    <li className="list-group-item">
+                                        Phone: {phone}
+                                    </li>
+                                </ul>
+                            ) : null}
+                        </div>
+                    );
+                }}
+            </Consumer>
         );
     }
 }
 
 Contact.propTypes = {
-    contact: PropTypes.object.isRequired,
-    deleteClickHandler: PropTypes.func.isRequired
+    contact: PropTypes.object.isRequired
 };
 
 export default Contact;
--- src/context.js
+++ src/context.js
@@ -2,6 +2,19 @@
 
 import React, { Component } from 'react';
 const Context = React.createContext();
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'DELETE_CONTACT':
+            return {
+                ...state,
+                contacts: state.contacts.filter(
+                    contact => contact.id !== action.payload
+                )
+            };
+        default:
+            return state;
+    }
+};
 
 export class Provider extends Component {
     state = {
@@ -24,7 +37,8 @@ export class Provider extends Component {
                 email: 'Katie@gmail.com',
                 phone: '0877957709'
             }
-        ]
+        ],
+        dispatch: action => this.setState(state => reducer(state, action))
     };
 
     render() {
