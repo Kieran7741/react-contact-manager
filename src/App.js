5efbdfe4d2bd341b8c80e742af5c8f50a732fe22
Kieran7741 <kieran7741@gmail.com>
2019-06-08 22:29:16 +0100

    added react-router-dom and added routes

--- src/App.js
+++ src/App.js
@@ -2,19 +2,31 @@ import React from 'react';
 import './App.css';
 import Contacts from './componets/contacts/Contacts';
 import Header from './componets/layout/Header';
+import About from './componets/pages/About';
 import { Provider } from './context';
 import AddContact from './componets/contacts/AddContact';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 function App() {
     return (
         <Provider>
-            <div className="App">
-                <Header branding="Contact Manager" />
+            <Router>
+                <div className="App">
+                    <Header branding="Contact Manager" />
 
-                <div className="container">
-                    <AddContact />
-                    <Contacts />
+                    <div className="container">
+                        <Switch>
+                            <Route exact path="/" component={Contacts} />
+
+                            <Route exact path="/about" component={About} />
+                            <Route
+                                exact
+                                path="/contact/add"
+                                component={AddContact}
+                            />
+                        </Switch>
+                    </div>
                 </div>
-            </div>
+            </Router>
         </Provider>
     );
 }
