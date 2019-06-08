b636c8810bcc7ccd5f46ab7f02e97f3a4e5c44c4
Kieran7741 <kieran7741@gmail.com>
2019-06-08 21:07:21 +0100

    added submit method to form

--- src/componets/contacts/AddContact.js
+++ src/componets/contacts/AddContact.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Consumer } from '../../context';
+import uuid from 'uuid';
 
 export default class AddContact extends Component {
     state = {
@@ -15,58 +17,83 @@ export default class AddContact extends Component {
         });
     };
 
-    onSubmit = event => {
+    onSubmit = (dispatch, event) => {
         event.preventDefault();
-        console.log(this.state);
+        const { name, email, phone } = this.state;
+        const newContact = {
+            id: uuid(),
+            name: name,
+            email: email,
+            phone: phone
+        };
+        dispatch({ type: 'ADD_CONTACT', payload: newContact });
+        this.setState({
+            name: '',
+            email: '',
+            phone: ''
+        });
     };
     render() {
         const { name, email, phone } = this.state;
+
         return (
-            <div className="card mb-3">
-                <div className="card-header">Add contact</div>
-                <div className="card-body">
-                    <form onSubmit={this.onSubmit}>
-                        <div className="form-group">
-                            <label htmlFor="name">Name</label>
-                            <input
-                                type="text"
-                                name="name"
-                                className="form-control form-control-lg"
-                                placeholder="Enter Name"
-                                value={name}
-                                onChange={this.onformChange}
-                            />
+            <Consumer>
+                {value => {
+                    const { dispatch } = value;
+                    return (
+                        <div className="card mb-3">
+                            <div className="card-header">Add contact</div>
+                            <div className="card-body">
+                                <form
+                                    onSubmit={this.onSubmit.bind(
+                                        this,
+                                        dispatch
+                                    )}
+                                >
+                                    <div className="form-group">
+                                        <label htmlFor="name">Name</label>
+                                        <input
+                                            type="text"
+                                            name="name"
+                                            className="form-control form-control-lg"
+                                            placeholder="Enter Name"
+                                            value={name}
+                                            onChange={this.onformChange}
+                                        />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="email">Email</label>
+                                        <input
+                                            type="text"
+                                            name="email"
+                                            className="form-control form-control-lg"
+                                            placeholder="Enter email"
+                                            value={email}
+                                            onChange={this.onformChange}
+                                        />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="phone">Phone</label>
+                                        <input
+                                            type="text"
+                                            name="phone"
+                                            className="form-control form-control-lg"
+                                            placeholder="Enter phone"
+                                            value={phone}
+                                            onChange={this.onformChange}
+                                        />
+                                    </div>
+                                    <input
+                                        type="submit"
+                                        value="Add Contant"
+                                        className="btn btn-light btn-block"
+                                    />
+                                </form>
+                            </div>
                         </div>
-                        <div className="form-group">
-                            <label htmlFor="email">Email</label>
-                            <input
-                                type="text"
-                                name="email"
-                                className="form-control form-control-lg"
-                                placeholder="Enter email"
-                                value={email}
-                                onChange={this.onformChange}
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="phone">Phone</label>
-                            <input
-                                type="text"
-                                name="phone"
-                                className="form-control form-control-lg"
-                                placeholder="Enter phone"
-                                value={phone}
-                                onChange={this.onformChange}
-                            />
-                        </div>
-                        <input
-                            type="submit"
-                            value="Add Contant"
-                            className="btn btn-light btn-block"
-                        />
-                    </form>
-                </div>
-            </div>
+                    );
+                }}
+            </Consumer>
         );
     }
 }
