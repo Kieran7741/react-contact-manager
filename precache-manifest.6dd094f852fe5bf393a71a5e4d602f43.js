76d3400f56bffec3b45617b0b4bcf3b87aaaf7ad
Kieran7741 <kieran7741@gmail.com>
2019-06-09 12:55:34 +0100

    Updates

---
+++ precache-manifest.6dd094f852fe5bf393a71a5e4d602f43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60a3ea1b2d399fbd6925668a2a0243a6",
+    "url": "/react-contact-manager/index.html"
+  },
+  {
+    "revision": "9086cf301468c51db335",
+    "url": "/react-contact-manager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "664816d4cc9128680f4b",
+    "url": "/react-contact-manager/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "9086cf301468c51db335",
+    "url": "/react-contact-manager/static/js/2.52bd020d.chunk.js"
+  },
+  {
+    "revision": "664816d4cc9128680f4b",
+    "url": "/react-contact-manager/static/js/main.f627f458.chunk.js"
+  },
+  {
+    "revision": "817909a50868ee2241bd",
+    "url": "/react-contact-manager/static/js/runtime~main.f3ab48b1.js"
+  }
+]);
